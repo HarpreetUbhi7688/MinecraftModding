bfc9b7266ae39bddd13555b9fccf1bc681d0feb2
Harpreet Ubhi <h.ubhi@hotmail.com>
2024-01-15 23:50:11 -0500

    Package changes/refactors. Changes to ErdreaMCMod.java. Changes to mods.toml
    Package Changes:
    - Changed 'com.example.examplemod' to 'com.harpreet.erdreamcmod' and right-clicked it to move/create to the correct directories/packages. Deleted the 'example' and 'examplemod' directories.
    - Refactored 'ExampleMod.java' to 'ErdreaMCMod.java'
    
    ErdreaMCMod.java:
    - Added a final String called 'MOD_ID' which has the hardcoded text, "erdreamcmod".
    - Replaced '@Mod("examplemod")' with '@Mod(ErdreaMCMod.MOD_ID)'.
    
    mods.toml:
    - Changed 'modId' to "erdreamcmod".
    - Changed 'displayName' to "Erdrea MC Mod".

--- src/main/java/com/harpreet/erdreamcmod/ErdreaMCMod.java
+++ src/main/java/com/harpreet/erdreamcmod/ErdreaMCMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.harpreet.erdreamcmod;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,13 +19,15 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod(ErdreaMCMod.MOD_ID)
+public class ErdreaMCMod
 {
+    public static final String MOD_ID = "erdreamcmod";
+
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public ErdreaMCMod() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
